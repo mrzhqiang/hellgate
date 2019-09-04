11ebaf159ed01d4da3f775a368f38b643ece48bb
张强 <qiang.zhang@touchair.cn>
2019-09-04 16:36:42 +0800

    更新：第一次提交

---
+++ src/main/java/com/github/mrzhqiang/hellgateapi/HellgateApiApplication.java
@@ -0,0 +1,12 @@
+package com.github.mrzhqiang.hellgateapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellgateApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HellgateApiApplication.class, args);
+  }
+}
